3316d9933089064b325dc935c3d8ff377b018e27
Scott Taylor <scott@railsnewbie.com>
2009-11-25 13:22:36 -0500

    Update initial schema migration

--- lib/migration_collapser/templates/initial_schema_migration.rb
+++ lib/migration_collapser/templates/initial_schema_migration.rb
@@ -1,2 +1,30 @@
 class InitialSchema < ActiveRecord::Migration
+  class << self
+    def up
+      class << connection = ActiveRecord::Base.connection
+        def username
+          @config[:username]
+        end
+
+        def host
+          @config[:host]
+        end
+      end
+
+      file = File.read(File.dirname(__FILE__) + "/initial_schema.sql")
+
+      file.split(";").each do |sql_line|
+        execute(sql_line)
+      end
+    end
+
+    def down
+    end
+
+  private
+
+    def drop_view(view_name)
+      execute "DROP VIEW IF EXISTS #{view_name}"
+    end
+  end
 end
