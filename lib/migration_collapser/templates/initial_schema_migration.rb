f4fa6c5fbff4d23c0de1b580e1f0646964833b39
Scott Taylor <scott@railsnewbie.com>
2009-11-25 14:11:32 -0500

    Update initial_schema.rb template file

--- lib/migration_collapser/templates/initial_schema_migration.rb
+++ lib/migration_collapser/templates/initial_schema_migration.rb
@@ -14,17 +14,57 @@ class InitialSchema < ActiveRecord::Migration
       file = File.read(File.dirname(__FILE__) + "/initial_schema.sql")
 
       file.split(";").each do |sql_line|
-        execute(sql_line)
+        execute(sql_line, true)
       end
     end
 
     def down
+      drop_views
+      drop_tables
     end
 
   private
 
-    def drop_view(view_name)
-      execute "DROP VIEW IF EXISTS #{view_name}"
+    def execute(sql_string, verbose = false)
+      if verbose
+        puts "* Executing: #{sql_string}"
+      end
+
+      ActiveRecord::Base.connection.execute(sql_string)
+    end
+
+    def drop_tables
+      tables.each do |table|
+        drop_table(table)
+      end
+    end
+
+    def drop_views
+      views.each do |view|
+        drop_view(view)
+      end
+    end
+
+    def drop_table(table_name)
+      puts "* Dropping table: #{table_name}"
+      execute "DROP TABLE `#{table_name}`"
+    end
+
+    def drop_view(table_name)
+      puts "* Dropping view: #{table_name}"
+      execute "DROP VIEW `#{table_name}`"
+    end
+
+    def tables
+      @tables ||= all_tables - ["schema_migrations"]
+    end
+
+    def all_tables
+      execute("SHOW FULL TABLES WHERE table_type = 'VIEW'").all_hashes.map { |x| x.values.last }
+    end
+
+    def views
+      @views ||= execute("SHOW FULL TABLES WHERE table_type = 'VIEW'").all_hashes.map { |x| x.values.last }
     end
   end
 end
